3f21f428ca2d0cea6e96639e0a4aa11cbf4fa49f
wooSangWook <ww57403@naver.com>
2022-12-29 01:50:41 +0900

    304. Implementing Bookmarks - Part 2

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -2,10 +2,11 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import paginationView from './views/paginationView.js';
 
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
@@ -23,6 +24,7 @@ const controlRecipes = async function () {
   try {
     //0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.laodRecipe(id);
@@ -70,11 +72,15 @@ const controlServings = function (updateTo) {
 };
 
 const coltrolAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
 
-  console.log(model.state.recipe);
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
